7c477e910bffe7d59efde74cd8bd911b9022604c
Divyansh Soni <divyanshsonijii@gmail.com>
2026-06-05 17:21:06 +0530

    About Page Added

--- rydex/src/lib/socket.ts
+++
@@ -1,9 +0,0 @@
-import { io, Socket } from "socket.io-client"
-
-let socket:Socket|null=null
-export const getSocket=()=>{
-    if(!socket){
-        socket=io(process.env.NEXT_PUBLIC_SOCKET_SERVER_SECRET)
-    }
-  return socket;
-}
